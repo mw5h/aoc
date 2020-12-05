9c0ad71a3db6a8e9ea2cf1717446c545cc83035a
Matt White <matt@bites.lan>
2020-12-05 09:32:01 -0800

    Shamelessly stole Alex's use of int(,2) and maketrans

--- 2020/day5.py
+++ 2020/day5.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python
 
 import sys
+import string
 
+trans = string.maketrans("fbrlFBRL", "01100110")
+data = [int(e.rstrip().translate(trans), 2) for e in sys.stdin]
 
-data = [e.rstrip() for e in sys.stdin]
-trans = {'F': 0, 'B': 1, 'R': 1, 'L': 0}
+print max(data)
 
-def seatNumber(spec):
-    seat = 0
-    for c in spec:
-        seat = seat * 2 + trans[c]
-    return seat
+seatset = frozenset(data)
 
-seats = [seatNumber(spec) for spec in data]
-
-print max(seats)
-
-seatset = frozenset(seats)
-
-print [x + 1 for x in seats if x + 2 in seatset and x + 1 not in seatset][0]
+print [x + 1 for x in data if x + 2 in seatset and x + 1 not in seatset][0]
